ff40f94a2436ee519f021d1d8d81b31631bac664
Unknown <sandramilenamoreno@ucundinamarca.edu.co>
2020-05-24 10:28:32 -0500

    ValidacionLote

--- Fruver/Fruver/Controller/LoteProducto.aspx.cs
+++ Fruver/Fruver/Controller/LoteProducto.aspx.cs
@@ -7,28 +7,7 @@ using System.Web.UI.WebControls;
 
 public partial class View_LoteProducto : System.Web.UI.Page
 {
-
-    protected void GV_InventarioProducto_RowUpdating(object sender, GridViewUpdateEventArgs e)
-    {
-        ClientScriptManager cm = this.ClientScript;
-        GridViewRow row = GV_InventarioProducto.Rows[e.RowIndex];
-        ELotes lotes = new ELotes();
- 
-        int cantidad = int.Parse(((TextBox)row.FindControl("TB_Cantidad")).Text);
-        double precio = double.Parse(((TextBox)row.FindControl("TB_Precio")).Text);
-
-        if (cantidad <= 0)
-        {
-            cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('La cantidad debe ser mayor a cero, no se admiten valores negativos ');</script>");
-            e.Cancel = true;
-        }else if (precio <= 0)
-        {
-            cm.RegisterClientScriptBlock(this.GetType(), "", "<script type='text/javascript'>alert('el precio debe ser mayor a cero, no se admiten valores negativos ');</script>");
-            e.Cancel = true;
-        }
-
-    }
-
+    
     protected void GV_InventarioProducto_RowDeleting(object sender, GridViewDeleteEventArgs e)
     {
         ClientScriptManager cm = this.ClientScript;
@@ -48,4 +27,5 @@ public partial class View_LoteProducto : System.Web.UI.Page
     {
         Response.Redirect("ReporteLote.aspx");
     }
+    
 }
